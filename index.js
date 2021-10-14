2ce22c29b12ee9faa2d1b4263d0868285b504a59
Cole Enyart <letchro@gmail.com>
2021-10-13 22:52:47 -0500

    Added requirements of files, Added questions to ask, Added layout for class employee

---
+++ index.js
@@ -0,0 +1,41 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./lib/Employee');
+const generateHtml = require('./src/generateHtml');
+
+const employee = new Employee();
+
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter the Name:  |',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'Enter the ID:    |',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'Enter the Email: |',
+        name: 'email',
+    }
+];
+
+function writeToFile(finalHtml) {
+    fs.writeFile('./dist/index.html', finalHtml, err => {
+        err ? console.log("error") : console.log("Success!!!")
+      })
+};
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((responses) => {
+            const finalHtml = generateHtml(responses);
+            writeToFile(finalHtml);
+        })
+};
+
+init();
