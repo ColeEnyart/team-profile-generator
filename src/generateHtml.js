978e89d7773389d3e2699b4794debc05ca2d518f
Cole Enyart <letchro@gmail.com>
2021-10-13 23:48:00 -0500

    Updated styling of generatedHtml

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -7,24 +7,31 @@ function generateHtml(data) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Team Profile</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="./style.css" />
     </head>
     <body>
+        <nav class="nav justify-content-center">
+            <span class="navbar-brand">My Team</span>
+        </nav>
+
         <div class="container mx-auto mt-4">
             <div class="row">
                 <div class="col-md-4">
                     <div class="card" style="width: 18rem;">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text mb-2">Card subtitle</p>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
-                            <a href="#" class="btn "><i class="fab fa-github"></i> Github</a>
+                            <h2 class="card-title">${data.name}</h2>
+                            <p class="card-text mb-2">ID: ${data.id}</p>
+                            <p class="card-text">Email: ${data.email}</p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>    
+        </div>
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
     </html>
 
