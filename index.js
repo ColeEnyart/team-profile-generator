f23fb6748e317307fe0c7c5092a02c3936ce5b36
Cole Enyart <letchro@gmail.com>
2021-10-15 17:56:03 -0500

    Added functions and questions for team members

--- index.js
+++ index.js
@@ -1,33 +1,97 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const generateHtml = require('./src/generateHtml');
 
-const managerInfo = [
-    {
-        type: 'input',
-        message: 'Enter the Name:  |',
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: 'Enter the ID:    |',
-        name: 'id',
-    },
-    {
-        type: 'input',
-        message: 'Enter the Email: |',
-        name: 'email',
-    },
-    {
-        type: 'input',
-        message: 'Enter the Office Number: |',
-        name: 'officeNumber',
-    }
-];
+function internInfo() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter the Name:  |',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Enter the ID:    |',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'Enter the Email: |',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Enter the School: |',
+            name: 'school',
+        }
+    ])
+    .then(res => {
+        const intern = new Intern (res);
+        generateHtml(intern);
+    })   
+}
+
+function engineerInfo() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter the Name:  |',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Enter the ID:    |',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'Enter the Email: |',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Enter the GitHub: |',
+            name: 'gitHub',
+        }
+    ])
+    .then(res => {
+        const engineer = new Engineer (res);
+        generateHtml(engineer);
+    })   
+}
+
+function managerInfo() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter the Name:  |',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Enter the ID:    |',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'Enter the Email: |',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Enter the Office Number: |',
+            name: 'officeNumber',
+        }
+    ])
+    .then(res => {
+        const manager = new Manager (res);
+        generateHtml(manager);
+    })   
+}
 
 function choice() {
     
